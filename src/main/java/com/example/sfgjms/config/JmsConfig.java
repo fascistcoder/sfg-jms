56172b499786b5a3481a74842e0c0dd962bbd4d7
Pulkit Aggarwal <pulkit.aggarwal@upgrad.com>
2022-03-21 11:16:50 +0530

    Message Converter Configuration

---
+++ src/main/java/com/example/sfgjms/config/JmsConfig.java
@@ -0,0 +1,24 @@
+package com.example.sfgjms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * @author <a href="pulkit.aggarwal@upgrad.com">Pulkit Aggarwal</a>
+ * @version 1.0
+ * @since 21/03/22
+ */
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter messageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return  converter;
+    }
+}
